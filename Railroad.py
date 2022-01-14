20f703dd270a74556c718a7a5a1736f92489cb42
gabrieljreed <gabrieljreed@gmail.com>
2022-01-13 17:50:15 -0700

    Code cleanup and add randomization

--- Railroad.py
+++ Railroad.py
@@ -1,66 +1,48 @@
 import maya.cmds as mc
+import random
 
 mc.select(all=True)
 mc.delete()
 
-# Track 
-trackL = mc.polyCube(name="trackL", w=30, h=1, d=0.3)
-trackR = mc.polyCube(name="trackR", w=30, h=1, d=0.3)
+# Track
+mc.polyCube(w=30, h=1, d=0.3)
+mc.move(0, 0.5, -1.8)
+mc.polyCube(w=30, h=1, d=0.3)
+mc.move(0, 0.5, 1.8)
 
-mc.move(0,0.5,-1.8, "trackL", relative=True)
-mc.move(0,0.5,1.8, "trackR", relative=True)
+for i in range(4):
+    mc.polyCube(w=30, h=0.2, d=0.2)
+    pos = i % 2
+    side = 1 - (i >= 2) * 2
+    mc.move(0, 0.1 + 0.8 * pos, 2 * side)  # There has to be a better way to write this
 
-mc.polyCube(w=30, h=0.2, d=0.2)
-mc.move(0,0.1,2)
-
-mc.polyCube(w=30, h=0.2, d=0.2)
-mc.move(0,0.1,-2)
-
-mc.polyCube(w=30, h=0.2, d=0.2)
-mc.move(0,0.9,2)
-
-mc.polyCube(w=30, h=0.2, d=0.2)
-mc.move(0,0.9,-2)
-
-
-# Slats 
+# Slats
 for i in range(10):
-    mc.polyCube(w=1, h=0.1, d=6)
-    mc.move(i*3 - 15, 0, 0)
+    slat = []
+    # Slats
+    scale = 6 + random.uniform(-0.1, 0.5)
+    cube = mc.polyCube(w=1, h=0.1, d=scale)
+    mc.polyBevel(o=0.02)
+    mc.move(i * 3 - 15, 0, 0)
+    slat.append(cube[0])
 
+    # Bolt plates
+    for j in range(2):
+        side = 1 - j * 2  # Determines what side of the track the plate should appear on
+        cube = mc.polyCube(w=0.9, h=0.1, d=0.6)
+        mc.polyBevel(o=0.02)
+        mc.move(i * 3 - 15, 0.1, 2.4 * side)
+        slat.append(cube[0])
 
-# Little thingies 
-for i in range(10):
-    mc.polyCube(w=1, h=0.1, d=0.6)
-    mc.move(i*3 - 15, 0.1, -2.4)
-    
-for i in range(10):
-    mc.polyCube(w=1, h=0.1, d=0.6)
-    mc.move(i*3 - 15, 0.1, 2.4)
+    # Bolts
+    for j in range(4):
+        sphere = mc.polySphere(r=0.1)
+        pos = 1 - (j % 2) * 2  # Determines the up or down position of the bolt
+        side = 1 - (j >= 2) * 2
+        mc.move(i * 3 - (15 + 0.2 * pos), 0.1, 2.4 * side)
+        slat.append(sphere[0])
 
-
-# Thingy Bolts
-for i in range(10):
-    mc.polySphere(r=0.1)
-    mc.move(i*3 - 15.2, 0.1, -2.4)
-    
-for i in range(10):
-    mc.polySphere(r=0.1)
-    mc.move(i*3 - 14.8, 0.1, -2.4)
-
-for i in range(10):
-    mc.polySphere(r=0.1)
-    mc.move(i*3 - 15.2, 0.1, 2.4)
-    
-for i in range(10):
-    mc.polySphere(r=0.1)
-    mc.move(i*3 - 14.8, 0.1, 2.4)
-
-
-# Track bolts 
-
-
-
-
-
-    
+    # Random rotation
+    mc.group(slat)
+    rot = random.uniform(0, 0.2)
+    mc.rotate(0, rot * 50 * ((rot * 100) % 2 - 1), 0)
